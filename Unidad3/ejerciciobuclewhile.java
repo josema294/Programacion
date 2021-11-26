a2ab0f10cff097c258b9a2c5ecf73499077aa071
josema294 <jmaranguren89@gmail.com>
2021-11-26 17:50:49 +0100

    addingwhilebucle

---
+++ Unidad3/ejerciciobuclewhile.java
@@ -0,0 +1,18 @@
+/**
+ * ejerciciobuclewhile
+ *Escribe un programa que muestre por consola los numero del 1 al 50 utiliznado un bucle while 
+ *Inicializa el numero al valor = 1
+ *Ten cuidado de no entrar en un bucle infinito
+ */
+public class ejerciciobuclewhile {
+
+    public static void main(String[] args) {
+
+        byte x=0;
+        while (x<50){
+            x++;
+            System.out.println(x);
+        }
+        
+    }
+}

e899c1e49382beb72a0ee8c1855dd7abe9241334
josema294 <jmaranguren89@gmail.com>
2022-01-06 17:22:53 +0100

    post vacaciones navidad

---
+++ Unidad4/Tarea1.java
@@ -0,0 +1,33 @@
+/*Considera una matriz ( array bidimensional) de 3 filas y 5 columnas
+
+1. Carga todos los valores de la matriz mediante la introduccion por consola de dichos valores
+
+2. Imprime el contenido de dicha matriz. */
+
+import java.util.Scanner;
+
+public class Tarea1 {
+
+   public static void main(String[] args) {
+
+        var matrix = new int [3][5]; 
+
+        Scanner input = new Scanner(System.in);
+
+        for (int fila = 0; fila < matrix.length; fila++){
+            for( int columna = 0; columna < matrix[fila].length; columna++){
+                matrix [fila][columna]= input.nextInt();
+            }
+
+        }
+
+        for (int fila = 0; fila < matrix.length; fila++){
+            
+            for( int columna = 0; columna < matrix[fila].length; columna++){
+                System.out.print("[" + matrix [fila][columna]+ "]");
+            }
+            System.out.println();    
+        }
+
+    }
+}

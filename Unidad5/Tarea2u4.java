bbd311aca4f6fed1780559438a0af17ef3846de0
josema294 <jmaranguren89@gmail.com>
2022-01-29 23:42:53 +0100

    Retoques y comprobaciones finales

---
+++ Unidad5/Tarea2u4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Tarea2u4 {
+
+    public static int aletorio (int min, int max){
+
+       double seed =  Math.random();
+
+       return (int)(seed*(max-min) + min);
+
+    }
+    
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce el numero minimo que quieres obtener");
+        int min = entrada.nextInt();
+        System.out.println("Introduce el numero maximo que quieres obtener");
+        int max = entrada.nextInt();
+
+        for (int i =0; i<=10; i++){
+        int random = aletorio (min,max);
+        System.out.println("Tu numero aleatorio comprendido en el rango dado es: " + random);}
+
+    }
+}

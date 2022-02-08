f868bb10c2698404b1d2a5506cb77ce1f3286b7e
josema294 <jmaranguren89@gmail.com>
2022-02-08 23:00:51 +0100

    Terminado el ejercicio 1 del tema 6 Cuenta

---
+++ Unidad6/Cuenta.java
@@ -0,0 +1,65 @@
+package Unidad6;
+//Tarea 1 ejercicio 6
+
+public class Cuenta {
+
+    private String titular;
+    private double cantidad;
+
+    //Creo el metodo constructor, con sobrecarga para que el mismo nombre tenga las dos opciones que nos piden.
+    Cuenta (String titular) {
+
+        this.titular = titular;
+        
+    }
+
+    Cuenta (String titular, double cantidad){
+        
+        this.cantidad = cantidad;
+        this.titular = titular;
+    }
+
+    //A continuacion metodos get set y toString 
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+   public String toString() {
+        return "Cuenta [el titular es: " + titular + " La cantidad depositada es: " +  cantidad + "]";
+    }
+
+    //Por ultimo los metodos para operar con la cuenta, retirar e ingresar.
+    public double ingresar (double ingreso){
+
+        if (ingreso>=0){
+            this.cantidad = this.cantidad + ingreso;
+        }
+        
+        return this.cantidad;
+    }
+    
+    public double retirar (double retirada){
+
+        if ((this.cantidad - retirada) >= 0) {
+            this.cantidad= this.cantidad - retirada;
+        }
+
+        else  {
+            this.cantidad = 0;
+        }   
+        
+        return this.cantidad;
+    }
+}
---
+++ Unidad6/MainCuentaTest.java
@@ -0,0 +1,34 @@
+package Unidad6;
+
+public class MainCuentaTest {
+
+    public static void main(String[] args) {
+        
+        //instanciamos objeto y comprobamos que getCantidad y Get titular nos lo devuelven bien.
+        Cuenta cuentajose = new Cuenta ("Jose", 2347.34);
+        System.out.println(cuentajose);
+        System.out.println(cuentajose.getCantidad());
+        System.out.println(cuentajose.getTitular());
+        
+        //Probamos setter cambiando datos
+        cuentajose.setTitular("Jose Maria");
+        cuentajose.setCantidad(3234.67);
+        System.out.println(cuentajose);
+        
+        //Operamos con los metodos retirar e ingrear y comprobamos que los outputs sean correctos.
+        cuentajose.retirar(5000);
+        System.out.println(cuentajose);
+        cuentajose.ingresar(3000);
+        System.out.println(cuentajose);
+        cuentajose.retirar(500);
+        System.out.println(cuentajose);
+        cuentajose.ingresar(-10);
+        System.out.println(cuentajose);
+        
+
+
+        
+
+    }
+    
+}

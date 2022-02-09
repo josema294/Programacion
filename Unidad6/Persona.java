87f01e8599e580a43e1f4dc07895d7ade121afd8
josema294 <jmaranguren89@gmail.com>
2022-02-09 22:52:55 +0100

    El super ejercicio 2 del tema 6, casi terminado

---
+++ Unidad6/Persona.java
@@ -0,0 +1,137 @@
+package Unidad6;
+
+public class Persona {
+
+    //Declaracion de atributos
+
+    private String nombre;
+    private int edad;
+    private String DNI;
+    private char sexo;
+    private double peso;
+    private double altura;
+    private final char sexodefecto = 'H';
+
+    public Persona (){ //constructor por defecto. 
+        
+        this.nombre = "";
+        this.edad = 0;
+        this.DNI = generaDNI();
+        this.sexo = sexodefecto;
+        this.peso = 0;
+        this.altura = 0;
+        
+    }
+
+    //Creacion de los otros 2 constructores que nos piden.
+
+    public Persona (String nombre, int edad, char sexo){
+        
+        this.nombre = nombre;
+        this.edad = edad;
+        this.DNI = generaDNI();
+        this.sexo = sexo;
+        this.peso = 0;
+        this.altura = 0;
+    }
+
+    public Persona (String nombre, int edad, char sexo, double peso, double altura ) {
+        
+        this.nombre = nombre;
+        this.edad = edad;
+        this.DNI = generaDNI();;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    //Implementando metodos 
+
+    public int calcularIMC() {
+
+        double IMC =  (peso/(altura*altura));
+        final int bajoimc = -1;
+        final int normalimc = 0;
+        final int altoimc = 1;
+
+        if (IMC<20){
+            return bajoimc;
+        }
+
+        else if (IMC>=20 && IMC<=25) {
+            return normalimc;
+        }
+
+        else {
+            return altoimc;
+        }
+
+    }
+    
+    public boolean esMayorDeEdad() {
+            
+        final int mayoriaDeEdad = 18;
+        if (edad >= mayoriaDeEdad){
+            return true;
+        }
+
+        else {return false;}
+        
+    }
+
+    private void comprobarSexo(char sexo) {
+
+        if (this.sexo != sexo){
+            this.sexo=sexodefecto;
+             
+        }
+    }
+
+    public String toString() {
+        return "Persona [DNI=" + DNI + ", altura=" + altura + ", edad=" + edad + ", nombre=" + nombre + ", peso=" + peso
+                + ", sexo=" + sexo + "]";
+    }
+
+     private String generaDNI(){
+        
+        int aleatoriodni = (int) Math.random()*99999999;
+
+        int resto = aleatoriodni %23;
+        char list [] = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
+
+        'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+
+        char letra = list[resto];
+
+        String falsodni = String.valueOf(aleatoriodni) + letra;
+
+        return falsodni;
+
+    }
+
+   public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+   
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    
+}
---
+++ Unidad6/PersonaEjecutable.java
@@ -0,0 +1,47 @@
+package Unidad6;
+
+import java.util.Scanner;
+
+public class PersonaEjecutable {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Introduce tu nombre");
+        String nombre = entrada.next();
+        System.out.println("Introduce tu edad");
+        int edad = entrada.nextInt();
+        
+        System.out.println("Introduce tu sexo H o M ");
+        String sexoentrada = entrada.next();
+        char sexo = sexoentrada.charAt(0);
+        System.out.println("Introduce tu peso en kg");
+        double peso = entrada.nextDouble();
+        System.out.println("Introduce tu altura en mts");
+        double altura = entrada.nextDouble();
+
+        Persona persona1 = new Persona(nombre, edad, sexo, peso, altura);
+        Persona persona2 = new Persona(nombre, edad, sexo);
+        Persona persona3 = new Persona();
+
+        persona3.setNombre("Manolito");
+        persona3.setEdad(32);
+        persona3.setSexo('H');
+        persona3.setPeso(76.5);
+        persona3.setAltura(1.75);
+
+        
+        System.out.println(persona1.calcularIMC());
+        System.out.println(persona2.calcularIMC());
+        System.out.println(persona3.calcularIMC());
+
+
+
+
+
+
+        
+    }
+    
+}

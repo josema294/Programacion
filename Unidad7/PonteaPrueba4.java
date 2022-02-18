eb5d76647967b46a63d76614ed35e9221e54971f
josema294 <jmaranguren89@gmail.com>
2022-02-19 00:36:19 +0100

    Primer ejercicio tema 7segundo

---
+++ Unidad7/PonteaPrueba4.java
@@ -0,0 +1,53 @@
+class Coche {
+    private String matricula;
+    private String color;
+    private String marca;
+    private String modelo;
+    private int anyoFabricacion;
+    private int antiguedad;
+      
+
+    public String getMatricula() {
+        return matricula;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public int getAnyoFabricacion() {
+        return anyoFabricacion;
+    }
+    public void setAnyoFabricacion(int anyoFabricacion) {
+        this.anyoFabricacion = anyoFabricacion;
+    }
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+   
+
+}
+
+
+public class PonteaPrueba4 {
+
+    public static void main(String[] args) {
+        
+    }
+}
